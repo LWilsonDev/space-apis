9a2f920dbea0d1b631aa116b8ad5719695f137e0
Lucy Leleu <lucyleleu@lucys-mbp.home>
2019-03-07 10:13:04 +0000

    urls for "/" fixed

--- space/urls.py
+++ space/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('space_station', views.space_station, name='space_station' ),
+    path('', views.space_station, name='space_station' ),
 
     path('get_location', views.get_location, name='get_location'),
 ]
--- spaceproject/urls.py
+++ spaceproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('space/', include('space.urls'))
+    path('', include('space.urls'))
 ]
